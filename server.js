2d31f9d3891e8d1bb7268582e084c91f40e2cb97
mp-web3 <mp.web3.t@gmail.com>
2023-12-10 11:48:45 +0100

    fixed api

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const fetchYouTubeData = require("./utils/fetchYouTubeData");
 const fetchNews = require("./utils/fetchNews");
 require("dotenv").config();
-require("./utils/cronJobs");
 
 fetchYouTubeData();
 fetchNews();
