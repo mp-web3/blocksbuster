18111120773c5e334cced4428ffc40af72f2a650
mp-web3 <mp.web3.t@gmail.com>
2023-12-26 09:57:30 +0100

    cleaned repo and added playsInline for video in Hero

--- app/components/hero/Hero.js
+++ app/components/hero/Hero.js
@@ -3,7 +3,7 @@ import text from "@/i18n/en/text.json";
 const HeroSection = () => {
     return (
       <div id="hero" className="hero-section">
-        <video className="hero-background-video" autoPlay loop muted>
+        <video className="hero-background-video" autoPlay loop muted playsInline>
           <source src="/Cube-Blockchain.mp4" type="video/mp4" />
         </video>
         <div className="hero-content">
